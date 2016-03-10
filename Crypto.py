853609581afdb6f642967db05feb5208091c1a0a
David Molinari <contact@davidmolinari.fr>
2016-03-10 23:19:33 +0100

    meh

---
+++ Crypto.py
@@ -0,0 +1,27 @@
+''' TP Cryoptographie
+Meh
+'''
+
+# A. Occurence d'une lettre dans un message :
+
+'''
+a. Lister les variables utilisées ainsi que leur type.
+message = variable de type chaine de caractere
+n = variable de type entier
+i et cpt = variables de type entier
+
+b. Il manque l'incrépentation du compteur i pour permettre de boucler
+jusqu'à la longueur de message, donc l'instruction : i = i + 1
+
+c. Cet algorithme permet de compter le nombre de fois où la lettre 'a'
+est dans le mesage.
+
+'''
+
+
+
+
+
+
+
+''' Programme Principal '''
